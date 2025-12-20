24db38f68a1680e2e4896d3c5b57aa602a1318ab
Ivan Ivanov <ivanov.github@gmail.com>
2025-12-20 19:54:59 +0200

    Added Category and Stock Transaction configuration classes

---
+++ InventoryManagment/Data/CategoryConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using InventoryManagment.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InventoryManagment.Data
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> category)
+        {
+            category
+                .HasKey(c => c.Id);
+        }
+    }
+}
---
+++ InventoryManagment/Data/StockTransactionsConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using InventoryManagment.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InventoryManagment.Data
+{
+    public class StockTransactionsConfiguration : IEntityTypeConfiguration<StockTransaction>
+    {
+        public void Configure(EntityTypeBuilder<StockTransaction> stockTransaction)
+        {
+            stockTransaction
+                .HasKey(st => st.Id);
+
+            stockTransaction
+                .HasOne(st => st.Product)
+                .WithMany(p => p.StockTransactions)
+                .HasForeignKey(st => st.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            stockTransaction
+                .HasQueryFilter(st => st.Product != null);
+
+            stockTransaction
+                .Property(st => st.CreatedAt)
+                .HasDefaultValue(DateTime.UtcNow)
+                .IsRequired();
+        }
+    }
+}
