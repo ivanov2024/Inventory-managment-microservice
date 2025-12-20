c1f6fc6b3ad0ac112afe6a4619abb15a63794a43
Ivan Ivanov <ivanov.github@gmail.com>
2025-12-20 17:02:13 +0200

    Added Category entity

---
+++ InventoryManagment/Data.Models/Product.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryManagment.Data.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(150)]
+        public string Description { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public virtual Category Category { get; set; } = null!;
+
+        public virtual ICollection<StockTransaction> StockTransactions { get; set; } 
+            = new HashSet<StockTransaction>();
+    }
+}
