46bb2f9972c88f4cef61b4285564c30878c44a3c
Ivan Ivanov <ivanov.github@gmail.com>
2026-01-02 18:39:32 +0200

    Added ICategoryService interface for category management
    Introduced the ICategoryService interface in the InventoryManagment.Services.Interfaces namespace. This interface provides async methods for CRUD operations on categories, utilizing Category and CategoryViewModel types.

---
+++ InventoryManagment/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using InventoryManagment.Data.Models;
+using InventoryManagment.Models;
+
+namespace InventoryManagment.Services.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> GetAllCategoriesAsync();
+
+        Task<Category> GetCategoryByIdAsync(int categoryId);
+
+        Task<bool> CreateCategoryAsync(CategoryViewModel categoryViewModel);
+
+        Task<bool> UpdateCategoryAsync(CategoryViewModel categoryViewModel, int categoryId);
+
+        Task<bool> DeleteCategoryAsync(int categoryId);
+    }
+}
