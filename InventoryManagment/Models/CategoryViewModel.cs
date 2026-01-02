54134dbdd4ee0f38135d8447eeb626003f76823f
Ivan Ivanov <ivanov.github@gmail.com>
2026-01-02 18:17:06 +0200

    Added a category view model

---
+++ InventoryManagment/Models/CategoryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryManagment.Models
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(50)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(150)]
+        public string Description { get; set; } = null!;
+    }
+}
